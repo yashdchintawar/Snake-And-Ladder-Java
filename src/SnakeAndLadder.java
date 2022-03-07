192626eb15d3b50e05ad9ac0c2cf4ccb0b1cd021
yashdchintawar <yashchintawar31@gmail.com>
2022-03-07 19:49:53 +0530

    Added rolling dice feature

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,8 +1,13 @@
 public class SnakeAndLadder {
     public static void main(String[] args) {
-        System.out.println("Snake And Ladder Game Is started");//welcome message
-
+        System.out.println("Snake And Ladder Game Is started");
+        int min=1;
+        int max=6;
         int user1 = 0;
+
         System.out.println("Player Position Is: "+user1);
+
+        int dice = (int) Math.floor(Math.random() * (max - min + 1) + min);
+        System.out.println("Player Dice Number Is: "+dice);
     }
 }
