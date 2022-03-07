e9ef38aae743b2191663607e3a0329a7b8739982
yashdchintawar <yashchintawar31@gmail.com>
2022-03-07 19:47:06 +0530

    UC01 User welcome message

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,7 +1,7 @@
 public class SnakeAndLadder {
     public static void main(String[] args) {
-        System.out.println("Snake And Ladder Game Is started");
-        
+        System.out.println("Snake And Ladder Game Is started");//welcome message
+
         int user1 = 0;
         System.out.println("Player Position Is: "+user1);
     }
